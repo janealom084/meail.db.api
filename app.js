ab54c47ed70952a80445e5b2d6201ccdcf6a4c46
janealom084 <janealom084@gmail.com>
2022-02-25 00:12:40 +0600

    template string added

--- app.js
+++ app.js
@@ -7,5 +7,27 @@ const searchFood = () => {
     // console.log(url)
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displySearchResult(data.meals))
+}
+
+const displySearchResult = meals => {
+    const searchResult = document.getElementById('search-result');
+    meals.forEach(meal => {
+        console.log(meal);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                This is a longer card with supporting text below as a natural lead-in to additional content.
+                This content is a little bit longer.
+                </p>
+            </div>
+        </div>
+        `
+        searchResult.appendChild(div);
+    })
 }
