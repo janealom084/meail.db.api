e637f4eeab23d3a6b316563c66bb08347b61df96
janealom084 <janealom084@gmail.com>
2022-02-25 00:43:00 +0600

    search result showed in website

--- app.js
+++ app.js
@@ -18,12 +18,11 @@ const displySearchResult = meals => {
         div.classList.add('col');
         div.innerHTML = `
         <div class="card">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">Dish title: ${meal.strMeal}</h5>
                 <p class="card-text">
-                This is a longer card with supporting text below as a natural lead-in to additional content.
-                This content is a little bit longer.
+                ${meal.strInstructions.slice(0, 100)}
                 </p>
             </div>
         </div>
